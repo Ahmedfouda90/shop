9ab7ac19b0fd0725ef973cd68ee248414181ea60
AhmedFouda <Ahmedfouda90@gmail.com>
2023-12-12 13:06:39 +0200

    main Commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const Shop());
+}
+class Shop extends StatelessWidget {
+  const Shop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // home: OnBoardingScreen(),
+    );
+  }
+}
